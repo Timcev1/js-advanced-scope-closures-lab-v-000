f881dd2201b3200a466603281135deb1908166a1
Timothy Cevallos <timcev@gmail.com>
2017-10-06 21:14:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function produceDrivingRange(blockRange){
   return function(a, b){
     let s = parseInt(a);
     let e = parseInt(b);
-    let distance = Math.abs(e - s);
-    let distance = blockRange - distanceToTravel;
+    let dis = Math.abs(e - s);
+    let dif = blockRange - distanceToTravel;
     if (difference > 0) {
       return `within range`
     }else {
